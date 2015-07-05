0bdd9916ad308f576b8e1f7722056f26a8e43423
Erick Ramirez <ramirezerick15@gmail.com>
2015-07-05 02:43:17 -0400

    update hero

--- ag_hero.rb
+++ ag_hero.rb
@@ -1,13 +1,27 @@
 #!/usr/bin/env ruby
 
 class Hero
-  def initialize(name, hp)
+  def initialize(name, hp, hero_class)
     @name = name
     @hp = hp
+    @hero_class = hero_class
+  end
+
+  def lower_hp(amount)
+    @hp -= amount
+  end
+
+  def raise_hp(amount)
+    if (@hp < 100)
+      @hp += amount
+    end
   end
 
   def status
-    puts "Name: " + @name
-    puts "HP: " + @hp.to_s
+    puts "+---- STATUS -----+"
+    puts "  Name: " + @name
+    puts "  HP: " + @hp.to_s
+    puts "  Class: " + @hero_class
+    puts "+---- STATUS -----+"
   end
 end
