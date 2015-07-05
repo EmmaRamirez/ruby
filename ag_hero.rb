0955475959093d700292aa0fd3ea585883189e1c
Erick Ramirez <ramirezerick15@gmail.com>
2015-07-05 03:12:17 -0400

    add level and exp

--- ag_hero.rb
+++ ag_hero.rb
@@ -1,9 +1,11 @@
 #!/usr/bin/env ruby
 
 class Hero
-  def initialize(name, hp, hero_class)
+  def initialize(name, hp, exp, level, hero_class)
     @name = name
     @hp = hp
+    @exp = exp
+    @level = level
     @hero_class = hero_class
   end
 
@@ -17,11 +19,20 @@ class Hero
     end
   end
 
+  def level_up
+    @level += 1
+  end
+
+  def raise_exp(amount)
+    @exp += exp
+  end
+
   def status
     puts "+---- STATUS -----+"
     puts "  Name: " + @name
-    puts "  HP: " + @hp.to_s
+    puts "  HP: " + @hp.to_s + " / 100"
     puts "  Class: " + @hero_class
+    puts "  Level: " + @level.to_s + " [" + @exp.to_s + "]"
     puts "+---- STATUS -----+"
   end
 end
