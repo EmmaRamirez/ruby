db3f86f560936ca4b8aa4b84c47158ad0a524658
Erick Ramirez <ramirezerick15@gmail.com>
2015-07-06 09:27:20 -0400

    handicap warrior class

--- adventure_game.rb
+++ adventure_game.rb
@@ -89,7 +89,7 @@ def ask_for_caste
       $name.set_class('Warrior')
       puts "Oh, a warrior! So you must really like strength."
       ensure_caste
-      $name.edit_powers(5, (-3), 0)
+      $name.edit_powers(3, (-5), 0)
     else
       reminder
       ask_for_caste
