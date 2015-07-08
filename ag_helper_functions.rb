6b2cf99cebc0dd6f713f7dc4482bd94062468c4e
Erick Ramirez <ramirezerick15@gmail.com>
2015-07-08 11:56:47 -0400

    add defs to helper funcs

--- ag_helper_functions.rb
+++ ag_helper_functions.rb
@@ -2,3 +2,16 @@ def enter
   print "[Press Enter]\n"
   enter = gets
 end
+
+def help
+  load_file("data/adventure_3.txt")
+  enter
+end
+
+def reminder
+  puts "Sorry, I didn't quite catch that... Please re-enter"
+end
+
+def check_status
+  $hero.status
+end
