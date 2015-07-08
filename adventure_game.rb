627fa73d2481813ea89e051db6001f56388f8b18
Erick Ramirez <ramirezerick15@gmail.com>
2015-07-08 12:33:29 -0400

    removed purchase functions

--- adventure_game.rb
+++ adventure_game.rb
@@ -6,6 +6,8 @@ require_relative 'ag_monster.rb'
 require_relative 'ag_helper_functions.rb'
 require_relative 'ag_explore.rb'
 require_relative 'ag_home.rb'
+require_relative 'ag_fight.rb'
+require_relative 'ag_market.rb'
 # require_relative 'ag_controls.rb'
 
 current_exploration = ''
@@ -27,71 +29,31 @@ load_file("data/adventure_1.txt")
 # block of text all at once...usually!
 # might just be the most important method in the game
 
-
-
-def are_you_sure?
-  while true
-    print "Correct? [y/n]: "
-    response = gets
-    case response
-    when /^[yY]/
-      return true
-    when /^[nN]/, /^$/
-      return false
-    end
-  end
-end
-
-def will_you_purchase_it?
-  while true
-    print "Will you purhcase it? [y/n]: "
-    response = gets
-    case response
-    when /^[yY]/
-      return true
-    when /^[nN]/, /^$/
-      return false
-    end
-  end
-end
-
-def get_name
-  $hero_name = gets.chomp
-  puts "Hmm... #{$hero_name}? What a strange name. Well, then let me ask you..."
-  enter
-end
-
-
 def ask_for_caste
   puts "What class do you belong to?"
   puts "[1] Mage [2] Knight [3] Warrior"
   caste = gets.chomp.downcase
-  if ($hero_name != nil)
-    $hero = Hero.new($hero_name, 100, 100, 0, 1, '', [], 50, 10, 10, 10)
-    if (caste == "1" or caste == "Mage".downcase)
-      $hero.set_class('Mage')
-      puts "Oh, a mage! So you must really like magic."
-      ensure_caste
-      $hero.edit_powers((-3), 5, 0)
-    elsif (caste == "2" or caste == "Knight".downcase)
-      $hero.set_class('Knight')
-      puts "Oh, a knight! So you must really like defense."
-      ensure_caste
-      $hero.edit_powers((-3), 0, 5)
-    elsif (caste == "3" or caste == "Warrior".downcase)
-      $hero.set_class('Warrior')
-      puts "Oh, a warrior! So you must really like strength."
-      ensure_caste
-      $hero.edit_powers(3, (-5), 0)
-    else
-      reminder
-      ask_for_caste
-    end
+  $hero = Hero.new($hero_name, 100, 100, 0, 1, '', [], 50, 10, 10, 10)
+  if (caste == "1" or caste == "Mage".downcase)
+    $hero.caste = "Mage"
+    puts "Oh, a mage! So you must really like magic."
+    ensure_caste
+    $hero.edit_powers((-3), 5, 0)
+  elsif (caste == "2" or caste == "Knight".downcase)
+    $hero.caste = "Knight"
+    puts "Oh, a knight! So you must really like defense."
+    ensure_caste
+    $hero.edit_powers((-3), 0, 5)
+  elsif (caste == "3" or caste == "Warrior".downcase)
+    $hero.caste = "Warrior"
+    puts "Oh, a warrior! So you must really like strength."
+    ensure_caste
+    $hero.edit_powers(3, (-5), 0)
+  else
+    reminder
+    ask_for_caste
   end
-  puts "Beginning adventure..."
-  enter
-  $hero.status
-  enter
+  begin
 end
 
 def ensure_caste
@@ -104,20 +66,6 @@ def ensure_caste
 end
 
 get_name
-ask_for_caste
-
-
-
-def add_item(item)
-  $hero.inventory_add(item.to_s)
-  puts "You acquired a #{item.downcase}!"
-end
-
-
-
-def calculate_bribe_cost
-  $hero.access_level * 100
-end
 
 
 
@@ -125,76 +73,21 @@ end
 
 
 
-def transaction(price, item)
-  if price > $hero.gems
-    puts "You can't afford this!"
-    return
-  else
-    $hero.edit_gems((price * -1))
-    add_item(item)
-  end
-end
 
-def ensure_purchase
-  if will_you_purchase_it? == true
-    return
-  else
-    puts "Clerk: Oh, I'm sorry! I didn't mean to show you the wrong item!"
-  end
-end
 
-def market_banner
-  puts "+~~~~~~~~~~~~~~~~~~~~~~~~~~~~+"
-  puts "|   Welcome to the market!   |"
-  puts "+~~~~~~~~~~~~~~~~~~~~~~~~~~~~+"
-end
 
-def market
-  seen_market = false
-  if seen_market == false
-    market_banner
-    seen_market = true
-  end
-  enter
-  puts "Clerk: How may I help you?"
-  puts "[1] Purchase [2] Sell"
-  response = gets.to_i
-  if response == 1
-    puts "Clerk: Well here's what we have in-stock today..."
-    puts "[1] Turtle Armor -- 50 gems\n[2] Leek Sword -- 70gems\n[3] Arizona Tea -- 80gems"
-    response = gets.to_i
-    if response == 1
-      puts "A tiny, but useful shield in the design of a turtle. Helps take hits."
-      ensure_purchase
-      transaction(50, "turtle armor")
-    elsif response == 2
-      puts "A sword, made out of a leek. Increases power against monsters."
-      ensure_purchase
-      transaction(70, "leek sword")
-    elsif response == 3
-      puts "A can of tea. Replenishes health during battle once."
-      transaction(80, "arizona tea")
-    end
-  elsif response == 2
-    puts "Current function not available."
-  else
-    "Sorry, I didn't quite catch that..."
-  end
 
-  # TODO: make this work
-  # puts "Are you done shopping? [y/n]"
-  # response = gets
-  # if response == /^[yY]/
-  #   "Clerk: Ok, have a great day!\nYou decided to return to your base camp."
-  #   home
-  # else
-  #   market
-  # end
 
-end
+
+
 
 
 
 explore
 
 home
+
+def add_item(item)
+  $hero.inventory_add(item.to_s)
+  puts "You acquired a #{item.downcase}!"
+end
--- ag_helper_functions.rb
+++ ag_helper_functions.rb
@@ -19,3 +19,35 @@ end
 def check_status
   $hero.status
 end
+
+def begin
+  puts "Beginning adventure..."
+  enter
+  check_status
+  enter
+end
+
+def are_you_sure?
+  while true
+    print "Correct? [y/n]: "
+    response = gets
+    case response
+    when /^[yY]/
+      return true
+    when /^[nN]/, /^$/
+      return false
+    end
+  end
+end
+
+def get_name
+  $hero_name = gets.chomp.to_s
+  if $hero_name != nil or $hero_name != ""
+    puts "Hmm... #{$hero_name}? What a strange name. Well, then let me ask you..."
+    ask_for_caste
+  else
+    puts "Please enter a valid name."
+    get_name
+  end
+  enter
+end
