4e1368112a7ba2dd430b47867cb23176fd8bb5bc
Erick Ramirez <ramirezerick15@gmail.com>
2015-07-08 13:03:33 -0400

    add more string methods

--- split_method.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ruby
-
-puts "Enter a sentence: "
-string = gets.to_s
-# splits between spaces
-arr = string.split(" ")
-puts arr
--- split_method_2.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ruby
-
-puts "Enter a sentence: "
-string = gets.to_s
-# splits entire sentence to letters
-arr = string.split("")
-puts arr
