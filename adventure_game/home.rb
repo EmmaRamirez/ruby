2d31b8a175a936784152fca7a2ad9a61c8bb7566
Erick <ramirezerick15@gmail.com>
2015-07-13 19:45:18 -0400

    add home function

--- adventure_game/home.rb
+++ adventure_game/home.rb
@@ -1,2 +1,12 @@
 def home
+  if $n_turns == 0
+    explore
+  else
+    puts "You decide to settle down for the time being. What will you do next?"
+    if $n_turns < 4
+      puts "[1] Explore [2] Market [3] Status [4] Rest"
+    elsif $n_turns > 4
+      puts "[3] Status [4] Rest"
+    end
+  end
 end
